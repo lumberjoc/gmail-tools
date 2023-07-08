c53cf7951dfd8221a69529c248a3e02893bf38e5
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2023-07-08 12:30:37 -0700

    update: input multiple emails

--- delete_by_sender.py
+++ delete_by_sender.py
@@ -5,7 +5,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
 # Define the email address of the sender you want to delete emails from
-sender_email = input("Enter sender email: ")
+sender_emails = input("Enter sender emails (separated by comma): ").split(",")
 
 # If modifying the scopes, delete the token.pickle file.
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
@@ -30,20 +30,30 @@ def delete_emails():
     creds = get_credentials()
     service = build('gmail', 'v1', credentials=creds)
     
-    query = f"from:{sender_email}"
-    results = service.users().messages().list(userId='me', q=query).execute()
-    messages = results.get('messages', [])
+    total_emails_moved = 0
     
-    if not messages:
-        print('No matching emails found.')
-        return
+    for sender_email in sender_emails:
+        query = f"from:{sender_email.strip()}"
+        results = service.users().messages().list(userId='me', q=query).execute()
+        messages = results.get('messages', [])
+        
+        if not messages:
+            print(f'No matching emails found for sender email: {sender_email}')
+            continue
+        
+        print(f'Moving {len(messages)} emails from sender: {sender_email.strip()} to trash...')
+        
+        batch = service.new_batch_http_request()
+        
+        for message in messages:
+            message_id = message['id']
+            batch.add(service.users().messages().modify(userId='me', id=message_id, body={'removeLabelIds': ['INBOX'], 'addLabelIds': ['TRASH']}))
+        
+        batch.execute()
+        
+        total_emails_moved += len(messages)
     
-    print(f'Deleting {len(messages)} emails...')
-    
-    for message in messages:
-        service.users().messages().modify(userId='me', id=message['id'], body={'removeLabelIds': ['INBOX']}).execute()
-    
-    print('Emails deleted successfully.')
+    print(f'Total emails moved to trash: {total_emails_moved}')
 
 delete_emails()
 
